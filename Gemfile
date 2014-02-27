e0241e847a9f197518fa2c7d065ab5e13743643b
Marcos Neves <marcos.neves@gmail.com>
2014-02-27 07:52:32 -0300

    utilizar a gem rails-i18n para traduzir as mensagens em português do Brasil

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+# Suporte a outros idiomas
+gem 'rails-i18n', '~> 4.0.0'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,9 @@ module HelpDesk
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.enforce_available_locales = true
-    config.i18n.default_locale = :br
-    config.i18n.fallbacks = {:br => :en}
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ['pt-BR', :en]
+    config.i18n.default_locale = 'pt-BR'
+    config.i18n.fallbacks = {'pt-BR' => :en}
   end
 end
