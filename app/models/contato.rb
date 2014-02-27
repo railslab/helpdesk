f97a6bbb06b7517399a455fe597d631f43f1f115
Marcos Neves <marcos.neves@gmail.com>
2014-02-27 12:50:35 -0300

    novas validações para o contato

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -1,3 +1,5 @@
 class Contato < ActiveRecord::Base
   validates :nome, :email, :mensagem, presence: true
+  validates :nome, :mensagem, length: {minimum: 3}
+  validates :email, format: /.+@.+/
 end
