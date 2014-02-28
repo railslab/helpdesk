794a0a71547902f32f9dbdcaeccd9ff860d28fc0
Marcos Neves <marcos.neves@gmail.com>
2014-02-28 04:21:03 -0300

    colocar classe text-muted na linha da tabela de contatos, quando o contato foi lido

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -2,4 +2,7 @@ class Contato < ActiveRecord::Base
   validates :nome, :email, :mensagem, presence: true
   validates :nome, :mensagem, length: {minimum: 3}
   validates :email, format: /.+@.+/
+
+  def lido?
+  end
 end
