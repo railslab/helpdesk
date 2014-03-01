f57859352f257b2d99c06797fc1a05d45520fd53
Marcos Neves <marcos.neves@gmail.com>
2014-03-01 18:43:06 -0300

    edição de usuário

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -7,12 +7,25 @@ class UsuariosController < ApplicationController
     @usuarios = Usuario.all
   end
 
+  def edit
+    @usuario = Usuario.find params[:id]
+  end
+
   def create
     @usuario = Usuario.create params.require(:usuario).permit(:nome, :email, :password, :password_confirmation)
     if @usuario.valid?
       redirect_to usuarios_path, notice: 'Usuário cadastrado com sucesso!'
     else
       render :new
+    end
+  end
+
+  def update
+    @usuario = Usuario.find params[:id]
+    if @usuario.update params.require(:usuario).permit(:nome, :email, :password, :password_confirmation)
+      redirect_to usuarios_path, notice: 'Usuário alterado com sucesso!'
+    else
+      render :edit
     end
   end
 end
