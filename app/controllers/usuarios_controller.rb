3f3f3e2c4466fae708b7998c7a702e21672b41b4
Marcos Neves <marcos.neves@gmail.com>
2014-03-01 00:35:34 -0300

    cadastro/lista de usuarios

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,2 +1,14 @@
 class UsuariosController < ApplicationController
+  def new
+    @usuario = Usuario.new
+  end
+
+  def index
+    @usuarios = Usuario.all
+  end
+
+  def create
+    Usuario.create params.require(:usuario).permit(:nome, :email, :senha)
+    redirect_to usuarios_path, notice: 'Usuário cadastrado com sucesso!'
+  end
 end
