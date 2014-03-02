a5281f5c6ca62d1d2c2fbfab1fdaa49c15217c0c
Marcos Neves <marcos.neves@gmail.com>
2014-03-01 21:42:33 -0300

    refatorar controller de usuarios

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -8,29 +8,44 @@ class UsuariosController < ApplicationController
   end
 
   def edit
-    @usuario = Usuario.find params[:id]
+    @usuario = model
   end
 
   def create
-    @usuario = Usuario.create params.require(:usuario).permit(:nome, :email, :password, :password_confirmation)
+    @usuario = Usuario.create form_params
     if @usuario.valid?
-      redirect_to usuarios_path, notice: 'Usuário cadastrado com sucesso!'
+      redirect_to_collection 'Usuário cadastrado com sucesso!'
     else
       render :new
     end
   end
 
   def update
-    @usuario = Usuario.find params[:id]
-    if @usuario.update params.require(:usuario).permit(:nome, :email, :password, :password_confirmation)
-      redirect_to usuarios_path, notice: 'Usuário alterado com sucesso!'
+    @usuario = model
+    if @usuario.update form_params
+      redirect_to_collection 'Usuário alterado com sucesso!'
     else
       render :edit
     end
   end
 
   def destroy
-    Usuario.find(params[:id]).destroy
-    redirect_to usuarios_path, notice: 'Usuário excluído!'
+    model.destroy
+    redirect_to_collection 'Usuário excluído!'
   end
+
+private
+
+  def model
+    Usuario.find params[:id]
+  end
+
+  def form_params
+    params.require(:usuario).permit(:nome, :email, :password, :password_confirmation)
+  end
+
+  def redirect_to_collection(notice)
+    redirect_to usuarios_path, notice: notice
+  end
+
 end
